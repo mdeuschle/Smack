04d35bf273dccc2251901cfe5278736013355bab
Matt Deuschle <mdeuschle@gmail.com>
2017-09-16 14:49:32 -0700

    rounded buttons

---
+++ Smack/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Matt Deuschle on 9/16/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
